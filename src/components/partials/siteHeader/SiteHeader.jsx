46c6350e15d218855281c38cee9da39fad426462
Huishan <hui_shan91@hotmail.com>
2022-10-27 17:39:11 +0800

    feat: edited footer styling

--- src/components/partials/siteHeader/SiteHeader.jsx
+++ src/components/partials/siteHeader/SiteHeader.jsx
@@ -1,6 +1,6 @@
 // import { useState } from "react";
 import { Link } from "react-router-dom";
-import logo from "../../../assets/images/Logo.png";
+import logo from "../../../assets/images/LogoS.png";
 import "./siteHeader.css";
 import { useNavigate, useLocation } from "react-router-dom";
 // import { toast } from "react-toastify";
