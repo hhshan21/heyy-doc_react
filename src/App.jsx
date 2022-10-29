cb30f5d10304c34a28bda5e7ff97509e0b159485
Huishan <hui_shan91@hotmail.com>
2022-10-29 16:12:58 +0800

    feat: updated all routes for project

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import Footer from "../src/components/partials/footer/Footer";
 import SiteHeader from "./components/partials/siteHeader/SiteHeader";
 import Homepage from "./components/homepage/Homepage";
 import Doctors from "./components/doctors/Doctors";
-import MyBookings from "./components/myBookings/MyBookings";
+import ShowBookings from "./components/bookings/showBookings/ShowBookings";
+import EditBookings from "./components/bookings/editBookings/EditBookings";
+import CreateBookings from "./components/bookings/createBookings/CreateBookings";
 import LoginPage from "./components/login/LoginPage";
 import RegisterPage from "./components/register/RegisterPage";
-import ShowProfile from "./components/showProfile/ShowProfile";
-import EditProfile from "./components/editProfile/EditProfile";
+import ShowProfile from "./components/profile/showProfile/ShowProfile";
+import EditProfile from "./components/profile/editProfile/EditProfile";
 import Auth from "./components/auth/Auth";
+import ShowDocAppointments from "./components/showDocAppointments/showDocAppointments";
 
 function App() {
   return (
@@ -21,15 +24,27 @@ function App() {
         <Route path="/doctors" element={<Doctors />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/my/profile" element={<Auth component={ShowProfile} />} />
         <Route
-          path="/users/my/profile"
-          element={<Auth component={ShowProfile} />}
-        />
-        <Route
-          path="/users/my/profile/edit"
+          path="/my/profile/edit"
           element={<Auth component={EditProfile} />}
         />
-        <Route path="/myBookings" element={<MyBookings />} />
+        <Route
+          path="/my/bookings"
+          element={<Auth component={ShowBookings} />}
+        />
+        <Route
+          path="/my/bookings/create"
+          element={<Auth component={CreateBookings} />}
+        />
+        <Route
+          path="/my/bookings/:id"
+          element={<Auth component={EditBookings} />}
+        />
+        <Route
+          path="/my/appointments"
+          element={<Auth component={ShowDocAppointments} />}
+        />
       </Routes>
       <Footer />
     </div>
