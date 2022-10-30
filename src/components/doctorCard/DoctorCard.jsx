907cb83409cd9754b0c46bdb89bd249fbb8b931e
Huishan <hui_shan91@hotmail.com>
2022-10-30 17:20:03 +0800

    feat: updated calling of doctor api

--- src/components/doctorCard/DoctorCard.jsx
+++ src/components/doctorCard/DoctorCard.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "./DoctorCard.css";
 
 import React from "react";
 
 const DoctorCard = (props) => {
-  console.log("props: ", props );
+  console.log("Doctor card props: ", props);
   return <div>DoctorCard</div>;
 };
 
--- src/pages/doctors/Doctors.jsx
+++ src/pages/doctors/Doctors.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Banner from "../../components/banner/BannerDoc";
+import "./Doctors.css";
 
-const Doctors = () => {
+const Doctors = (props) => {
+  useEffect(() => {
+    const fetchApi = async () => {
+      const res = await fetch("http://localhost:8000/api/v1/doctors");
+      const data = await res.json();
+      console.log("data: ", data);
+    };
+    fetchApi();
+  }, []);
+
   return (
     <div>
       <Banner></Banner>Meet our Panel of Doctors
