a3a9cd22b028253121bd5aa30baa8a357bf72727
Huishan <hui_shan91@hotmail.com>
2022-10-30 18:27:52 +0800

    feat: successfully rendered doc info into doctor card and made adjustment to SiteHeader

--- src/components/doctorCard/DoctorCard.jsx
+++ src/components/doctorCard/DoctorCard.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./DoctorCard.css";
 
-import React from "react";
-
 const DoctorCard = (props) => {
-  console.log("Doctor card props: ", props);
-  return <div>DoctorCard</div>;
+  console.log("DoctorCard props: ", props);
+  const { id, firstName } = props.data;
+
+  return <div>DoctorCard - {firstName} </div>;
 };
 
 export default DoctorCard;
--- src/pages/doctors/Doctors.jsx
+++ src/pages/doctors/Doctors.jsx
@@ -1,20 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Banner from "../../components/banner/BannerDoc";
+import DoctorCard from "../../components/doctorCard/DoctorCard";
 import "./Doctors.css";
 
 const Doctors = (props) => {
+  const [doctors, setDoctors] = useState([]);
+
   useEffect(() => {
     const fetchApi = async () => {
       const res = await fetch("http://localhost:8000/api/v1/doctors");
       const data = await res.json();
-      console.log("data: ", data);
+      // console.log("data: ", data);
+      setDoctors(data);
     };
     fetchApi();
   }, []);
 
+  const doctorCards = doctors.map((doctor) => (
+    <DoctorCard key={doctor.id} data={doctor} />
+  ));
+
   return (
     <div>
       <Banner></Banner>Meet our Panel of Doctors
+      {doctorCards}
     </div>
   );
 };
