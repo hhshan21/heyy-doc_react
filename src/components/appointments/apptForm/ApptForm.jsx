3dbc110a908510e47b66a941481af57b279710c5
Huishan <hui_shan91@hotmail.com>
2022-11-02 18:11:36 +0800

    feat: updated some formatting

--- src/components/appointments/apptForm/ApptForm.jsx
+++ src/components/appointments/apptForm/ApptForm.jsx
@@ -15,24 +15,26 @@ const ApptForm = (props) => {
   const navigate = useNavigate();
 
   const doctors = props.info;
+  console.log("doctors: ", doctors);
+  // const docInfo = doctors.map((doctor) => )
+
+  // to get doctor's name
   const docFirstName = doctors.map((ele) => ele.firstName);
-  console.log("docFirstName: ", docFirstName);
   const docLastName = doctors.map((ele) => ele.lastName);
-  console.log("docLastName: ", docLastName);
   const docName = docLastName.map(
     (ele, ind) => "Dr. " + ele + " " + docFirstName[ind]
   );
-  console.log("docName: ", docName);
-  const doctorTime = doctors.map((ele) => ele.doctorTime);
-  console.log("doctorTime: ", doctorTime);
-  const slot = doctorTime.forEach((slot) => console.log("slot: ", slot));
-  // console.log("slot: ", slot);
+
+  // to get doctor's time
+  // const doctorTime = doctors.map((ele) => ele.doctorTime);
+  // console.log("doctorTime: ", doctorTime);
+  // const slot = doctorTime.forEach((slot) => console.log("slot: ", slot));
 
   const tomorrow = DateTime.now()
     .plus({ days: 1 })
     .setLocale("zh")
     .toLocaleString();
-  console.log("tomorrow: ", tomorrow);
+
   const [docFullName, setDocFullName] = useState(docName[0]);
   const [apptDate, setApptDate] = useState(tomorrow);
 
@@ -44,11 +46,6 @@ const ApptForm = (props) => {
 
   // form validation rules
   const validationSchema = yup.object().shape({
-    // firstName: yup.string().min(1, "Mininum 1 character").required(),
-    // lastName: yup.string().min(2, "Mininum 2 characters").required(),
-    // appointmentDate: yup
-    //   .date()
-    //   .default(() => new Date(currentDate.getTime() + 86400000)),
     symptoms: yup
       .string()
       .min(3, "Mininum 3 characters")
@@ -57,9 +54,9 @@ const ApptForm = (props) => {
 
   //actual input names
   const defaultValues = {
-    firstName: "",
-    lastName: "",
-    // appointmentDate: new Date(currentDate.getTime() + 86400000),
+    // firstName: "",
+    // lastName: "",
+    // bookingDate: new Date(currentDate.getTime() + 86400000),
     appointmentTime: "",
     symptoms: "",
   };
@@ -154,6 +151,7 @@ const ApptForm = (props) => {
                   label={"Symptoms:"} //label in the box
                   variant="outlined"
                   rows={4}
+                  required
                   //multiline={true}
                   fullWidth
                   autoFocus
@@ -168,88 +166,6 @@ const ApptForm = (props) => {
               )}
             />
           </Box>
-          {/* <Box mb={3}>
-            <Controller
-              name="firstName" //actual input
-              control={control} //take place of the register RHF
-              render={({
-                //takes a function and return a react element
-                field, //this error will be displyed in formstate errors
-              }) => (
-                <TextField
-                  // onChange={onChange} // send value to hook form
-                  // value={value}
-                  label={"Doctor's Name:"} //label in the box
-                  variant="outlined"
-                  fullWidth
-                  autoComplete="firstName"
-                  autoFocus
-                  // error={!!error} //convert obj into a bool
-                  // helperText={error ? error.message : null}
-                  error={errors.firstName ? true : false}
-                  helperText={errors.firstName?.message}
-                  {...field}
-                />
-              )}
-            />
-          </Box> */}
-          {/* <Box mb={3}>
-            <Controller
-              control={control}
-              name={doc}
-              render={({ field: { onChange, value } }) => (
-                <select onChange={onChange} value={value}>
-                  {docNameOptions()}
-                </select>
-              )}
-            />
-          </Box> */}
-          {/* <Box mb={3}>
-            <Controller
-              name="lastName" //actual input
-              control={control} //take place of the register RHF
-              render={({
-                field, //this error will be displyed in formstate errors
-              }) => (
-                <TextField
-                  label={"Last Name:"} //label in the box
-                  variant="outlined"
-                  fullWidth
-                  autoComplete="lastName"
-                  autoFocus
-                  // error={!!error} //convert obj into a bool
-                  // helperText={error ? error.message : null}
-                  error={errors.lastName ? true : false}
-                  helperText={errors.lastName?.message}
-                  {...field}
-                />
-              )}
-            />
-          </Box> */}
-
-          {/* <Box mb={3}>
-            <Controller
-              name="bookingDate" //actual input
-              control={control} //take place of the register RHF
-              render={({
-                field, //this error will be displyed in formstate errors
-              }) => (
-                <TextField
-                  label={"Select your appointment Date"} //label in the box
-                  variant="outlined"
-                  fullWidth
-                  autoComplete="bookingDate"
-                  autoFocus
-                  // error={!!error} //convert obj into a bool
-                  // helperText={error ? error.message : null}
-                  error={errors.bookingDate ? true : false}
-                  helperText={errors.bookingDate?.message}
-                  {...field}
-                />
-              )}
-            />
-          </Box> */}
-
           <div className="apptFormBtn">
             <Button
               onClick={handleCancel}
