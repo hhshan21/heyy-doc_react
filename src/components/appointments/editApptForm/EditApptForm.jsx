bbdf9e3d46efca1e427178913a37a334fb635a6c
Huishan <hui_shan91@hotmail.com>
2022-11-04 00:35:37 +0800

    feat: successfully rendered out the appt form but without original data

--- src/components/appointments/editApptForm/EditApptForm.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./EditApptForm.css";
-
-const EditApptForm = (props) => {
-  console.log("props in edit: ", props);
-  return <div>Edit Bookings</div>;
-};
-
-export default EditApptForm;
