4e7191167dfd30b07da91795ebad613ac42df19c
Huishan <hui_shan91@hotmail.com>
2022-11-05 04:08:13 +0800

    feat: updated css styling

--- src/pages/home/Homepage.jsx
+++ src/pages/home/Homepage.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 import Banner from "../../components/banner/BannerMain";
+import HealthIcon from "../../assets/images/healthIcon.png";
+import Calender from "../../assets/images/calender.png";
+import Healthcare from "../../assets/images/healthcare.png";
+import Wordings from "./Wordings";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./Homepage.css";
+import "bootstrap";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
+      <Wordings></Wordings>
+      <Container>
+        <Row>
+          <Col>
+            <img className="healthIcon" src={HealthIcon} alt="HealthIcon" />
+          </Col>
+          <Col>
+            <img className="calender" src={Calender} alt="HealthIcon" />
+          </Col>
+          <Col>
+            <img className="healthcare" src={Healthcare} alt="HealthIcon" />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
---
+++ src/pages/home/Wordings.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Wordings.css";
+
+const Wordings = () => {
+  return (
+    <div className="wordings">
+      BOOK AN APPOINTMENT WITH ANY DOCTOR IN SINGAPORE WITH
+      <p className="heyDoc">HEYY DOC!</p>
+      <p>Access a full list of doctors at your own time.</p>
+      <p className="heyDoc">Heyy Doc! Anywhere, anytime.</p>
+    </div>
+  );
+};
+
+export default Wordings;
